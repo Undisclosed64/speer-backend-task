6197e5e0a658c6ed194afb7b3be1c6d04a3386e2
undisclosed64 <undisclosed64@gmail.com>
2024-01-04 05:22:14 -0800

    use notes_router

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const userRouter = require("./routes/user");
+const notesRouter = require("./routes/notes");
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_URI, {
@@ -12,7 +13,7 @@ const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api", userRouter);
+app.use("/api", userRouter, notesRouter);
 
 const port = process.env.PORT || 5000;
 
