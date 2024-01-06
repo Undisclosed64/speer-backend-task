59e3fae1759b98f394d3e45ff3c3f711ee57e678
undisclosed64 <undisclosed64@gmail.com>
2024-01-05 23:41:59 -0800

    wrap App in context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import { Outlet } from "react-router-dom";
+import { NoteProvider } from "./NoteContext";
 
 const App = () => {
   return (
-    <div className="bg-brightWhite flex">
-      <Sidebar />
-      <div className="flex-grow flex-shrink-0 ml-16">
-        <Outlet />
+    <NoteProvider>
+      <div className="bg-brightWhite flex">
+        <Sidebar />
+        <div className="flex-grow flex-shrink-0 ml-16">
+          <Outlet />
+        </div>
       </div>
-    </div>
+    </NoteProvider>
   );
 };
 
