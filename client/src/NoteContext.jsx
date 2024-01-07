4b9e24f3abdfdaec81bda17752f8f642bf83639f
undisclosed64 <undisclosed64@gmail.com>
2024-01-06 23:53:32 -0800

    update notecontext

--- client/src/NoteContext.jsx
+++ client/src/NoteContext.jsx
@@ -11,7 +11,7 @@ export const NoteProvider = ({ children }) => {
     try {
       const headers = {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvaG4iLCJlbWFpbCI6ImpvaG5AZ21haWwuY29tIiwiaWQiOiI2NTk4YzlmMTI2ZDVkNjhmZTE4ZTYyZGYiLCJpYXQiOjE3MDQ1MTE5OTgsImV4cCI6MTcwNDU0Nzk5OH0.FX3BbD84JxvvLj6_PsUQggIiwD3OrSIhs0xcgMfdJkE",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkpvaG4iLCJlbWFpbCI6ImpvaG5AZ21haWwuY29tIiwiaWQiOiI2NTk4YzlmMTI2ZDVkNjhmZTE4ZTYyZGYiLCJpYXQiOjE3MDQ2MDcxMTEsImV4cCI6MTcwNDY0MzExMX0.hzKvGu8BZzZd5dw48xVrtvxDiO0-5pMlOYTp5OgYxp4",
       };
 
       const response = await axios.get("http://localhost:5000/api/notes", {
@@ -33,7 +33,7 @@ export const NoteProvider = ({ children }) => {
   };
 
   return (
-    <NoteContext.Provider value={{ notes, addNote, fetchNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, fetchNotes, setNotes }}>
       {children}
     </NoteContext.Provider>
   );
