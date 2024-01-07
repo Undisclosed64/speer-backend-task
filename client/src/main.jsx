f9d8c4dc7eaaee30cad7b96d454e0eefeb46b9fb
undisclosed64 <undisclosed64@gmail.com>
2024-01-06 23:54:02 -0800

    add path

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import Note from "./components/Note.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Notes from "./components/Notes.jsx";
+import SharedNoteView from "./components/SharedNoteView.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
         element: <Note />,
       },
     ],
+  },
+  {
+    path: "/notes/shared/:id",
+    element: <SharedNoteView />,
   },
 ]);
 
