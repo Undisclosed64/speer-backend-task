a5d3bbcf815383f2e26297fa841a655ed1e0475e
undisclosed64 <undisclosed64@gmail.com>
2024-01-06 23:49:37 -0800

    add search for notes route

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const notesController = require("../controllers/notesController");
 const verify = require("../verifyUser");
 
+router.get("/notes/search", notesController.noteByKeyword);
+
 router.post("/notes", verify, notesController.createNote);
 
 router.get("/notes", verify, notesController.getAllNotes);
@@ -12,5 +14,9 @@ router.get("/notes/:id", verify, notesController.getNote);
 router.put("/notes/:id", verify, notesController.updateNote);
 
 router.delete("/notes/:id", verify, notesController.deleteNote);
+
+router.post("/notes/:id/share", verify, notesController.shareNote);
+
+router.get("/notes/shared/:id", notesController.sharedNoteViewer);
 
 module.exports = router;
