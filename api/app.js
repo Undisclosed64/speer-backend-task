1b1b5020a7187f642fd164098818f878cb54eaa9
undisclosed64 <undisclosed64@gmail.com>
2024-01-08 09:59:27 -0800

    add cors options

--- api/app.js
+++ api/app.js
@@ -12,7 +12,15 @@ mongoose.connect(process.env.MONGODB_URI, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+  origin: "https://scribe-note-taker.vercel.app",
+  methods: "GET,PUT,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", userRouter, notesRouter);
 
